eaeaf01d4fa497d02a1bfa13c0d1a5a9431f533e
Simon Bermejo Mendez <simonbermejomendez@MacBook-Air-de-Simon.local>
2022-09-16 16:05:23 +0200

    Excel dividido intento 1

--- DIVIDER.py
+++ DIVIDER.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
-df = pd.read_excel("datos_EXCEL_NORM")
+df = pd.read_excel("datos_EXCEL_NOR.xlsx")
+
+p_train = 0.80 # Porcentaje de train.
+
+train = df[:int((len(df))*p_train)] 
+test = df[int((len(df))*p_train):]
+
+
+print(type(test))
